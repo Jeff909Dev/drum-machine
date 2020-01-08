f4ad0e730241780818fa83a3e874c125f0d9ab17
Jeffrey Viveros <temel57@grupotemel.net>
2020-01-08 13:10:41 +0100

    Updates

---
+++ precache-manifest.0c2dc15dfa356bad3dd4b22f340abbac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ccafa5736c975adbcf89883203d3ad",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "ec156b3f5a3e1a755b45",
+    "url": "/drum-machine/static/css/main.6f65a182.chunk.css"
+  },
+  {
+    "revision": "a7b190610d2024e5e391",
+    "url": "/drum-machine/static/js/2.da8f7596.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/drum-machine/static/js/2.da8f7596.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec156b3f5a3e1a755b45",
+    "url": "/drum-machine/static/js/main.c2a1f204.chunk.js"
+  },
+  {
+    "revision": "8812de4e5e4091b01bac",
+    "url": "/drum-machine/static/js/runtime-main.c8eb23b9.js"
+  }
+]);
